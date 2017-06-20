27d939a947bdfa30a5f45cabff6a45d27c9dc59e
xiaochao <13942156310@163.com>
2017-06-20 13:26:07 +0800

    修改目录结构，项目可以运行

--- src/com/servlet/HelloWorld.java
+++ src/com/servlet/HelloWorld.java
@@ -6,14 +6,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name = "HelloWorld")
+@WebServlet(name="hello", urlPatterns = {"/hello"})
 public class HelloWorld extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        PrintWriter writer = response.getWriter();
+        writer.println("6666");
     }
 }
